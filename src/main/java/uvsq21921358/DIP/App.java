8d3e0721ffe6ac506483ad4efd53d8f40dfd5997
Utilisateur <user@debian.arcahe.ovh>
2020-03-03 17:25:59 +0100

    Le principe DIP

---
+++ src/main/java/uvsq21921358/DIP/App.java
@@ -0,0 +1,13 @@
+package uvsq21921358.DIP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
